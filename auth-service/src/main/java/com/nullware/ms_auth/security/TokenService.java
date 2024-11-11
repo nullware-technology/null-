54a364fc5937704851b782e4b51de4f355bffc57
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-11 16:08:24 -0300

    feat: added TokenService

---
+++ auth-service/src/main/java/com/nullware/ms_auth/security/TokenService.java
@@ -0,0 +1,53 @@
+package com.nullware.ms_auth.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.nullware.ms_auth.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret.getBytes());
+
+            return JWT.create()
+                    .withIssuer("ms-auth")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while authenti" +
+                    "cating");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret.getBytes());
+            return JWT.require(algorithm)
+                    .withIssuer("ms-auth")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
