62bff4d2375f482293cb646e2a5789b2c7519100
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-11 20:42:08 -0300

    feat: added login and refresh token route

--- auth-service/src/main/java/com/nullware/ms_auth/controllers/impl/AuthControllerImpl.java
+++ auth-service/src/main/java/com/nullware/ms_auth/controllers/impl/AuthControllerImpl.java
@@ -1,43 +1,52 @@
-package com.nullware.ms_auth.controllers;
+package com.nullware.ms_auth.controllers.impl;
 
+import com.nullware.ms_auth.controllers.AuthController;
 import com.nullware.ms_auth.dtos.requests.ForgotPasswordDTO;
 import com.nullware.ms_auth.dtos.requests.LoginDTO;
 import com.nullware.ms_auth.dtos.requests.RefreshTokenDTO;
 import com.nullware.ms_auth.dtos.requests.ResetPasswordDTO;
 import com.nullware.ms_auth.dtos.responses.GenericResponseDTO;
-import com.nullware.ms_auth.dtos.responses.LoginResponseDTO;
-import com.nullware.ms_auth.dtos.responses.RefreshTokenResponseDTO;
+import com.nullware.ms_auth.dtos.responses.TokenResponseDTO;
 import com.nullware.ms_auth.dtos.responses.ResetPasswordResponseDTO;
+import com.nullware.ms_auth.security.TokenService;
 import com.nullware.ms_auth.services.AuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AuthControllerImpl implements AuthController {
 
     private final AuthService authService;
+    private final TokenService tokenService;
 
     @Override
-    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginDTO loginDTO) {
-        LoginResponseDTO response = authService.login(loginDTO);
+    public ResponseEntity<TokenResponseDTO> login(@RequestBody @Valid LoginDTO loginDTO) {
+        TokenResponseDTO response = authService.login(loginDTO);
         return ResponseEntity.ok(response);
     }
 
     @Override
-    public ResponseEntity<RefreshTokenResponseDTO> refreshToken(@RequestBody @Valid RefreshTokenDTO refreshTokenDTO) {
-        RefreshTokenResponseDTO response = authService.refreshToken(refreshTokenDTO);
-        return ResponseEntity.ok(response);
-    }
+    public ResponseEntity<TokenResponseDTO> refreshToken(@RequestBody @Valid RefreshTokenDTO refreshTokenDTO) {
+        String refreshToken = refreshTokenDTO.refreshToken();
+        log.info("Received request to refresh access token: {}", refreshToken);
 
-    @Override
-    public ResponseEntity<GenericResponseDTO> logout() {
-        GenericResponseDTO response = authService.logout();
-        return ResponseEntity.ok(response);
+        TokenResponseDTO tokenResponseDTO = tokenService.refreshAccessToken(refreshTokenDTO.refreshToken());
+
+        if (tokenResponseDTO.accessToken() == null) {
+            log.error("Failed to refresh access token: {}", refreshToken);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        log.info("Refresh token is valid, generating new access token");
+
+        log.info("New access token generated successfully");
+        return ResponseEntity.ok(tokenResponseDTO);
     }
 
     @Override
