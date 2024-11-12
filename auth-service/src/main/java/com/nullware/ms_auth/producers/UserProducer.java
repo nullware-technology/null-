9ca28c393ecc764869a5ea325df8dbdb922887e2
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-11 21:49:03 -0300

    refactor: wrong docs

--- auth-service/src/main/java/com/nullware/ms_auth/producers/UserProducer.java
+++ auth-service/src/main/java/com/nullware/ms_auth/producers/UserProducer.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
- * Produces messages for user registration and password recovery.
+ * Produces messages for password recovery.
  *
  * @see #publishRecoverPasswordMessageEmail(User, String)
  */
