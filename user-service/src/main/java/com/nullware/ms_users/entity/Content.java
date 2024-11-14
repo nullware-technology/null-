7eda07193b5aacff83001a8ae81ddeeb1daf9828
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-13 23:03:25 -0300

    feat: added Content

---
+++ user-service/src/main/java/com/nullware/ms_users/entity/Content.java
@@ -0,0 +1,31 @@
+package com.nullware.ms_users.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Content {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column(name = "age_rating")
+    private Integer ageRating;
+
+    @OneToMany(mappedBy = "content", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<History> historyRecords;
+}
