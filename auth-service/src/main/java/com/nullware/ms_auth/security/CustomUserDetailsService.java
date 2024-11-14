241030e550dc5517d8287926b8de96f50f23b6a9
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-13 22:34:10 -0300

    refactor: authorities by subscription plan

--- auth-service/src/main/java/com/nullware/ms_auth/security/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/nullware/ms_auth/security/CustomUserDetailsService.java
@@ -1,14 +1,12 @@
 package com.nullware.ms_auth.security;
 
 import com.nullware.ms_auth.entity.User;
+import com.nullware.ms_auth.models.Plans;
 import com.nullware.ms_auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-
-import java.util.Collections;
 
 @Component
 @RequiredArgsConstructor
@@ -22,7 +20,7 @@ public class CustomUserDetailsService {
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
                 user.getPassword(),
-                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+                Plans.getAuthoritiesByPlan(user.getSubscriptionPlanName())
         );
     }
 }
