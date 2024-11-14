ba81c9d0c3b2088ca8467d073f751d877bbbd399
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-13 23:04:05 -0300

    feat: added Plans

---
+++ user-service/src/main/java/com/nullware/ms_users/models/Plans.java
@@ -0,0 +1,28 @@
+package com.nullware.ms_users.models;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+public class Plans {
+    public static final String ROLE_BASIC = "ROLE_BASIC";
+    public static final String ROLE_PREMIUM = "ROLE_PREMIUM";
+
+    public static final List<SimpleGrantedAuthority> BASIC_AUTHORITIES = List.of(
+            new SimpleGrantedAuthority("ROLE_USER"),
+            new SimpleGrantedAuthority(ROLE_BASIC)
+    );
+
+    public static final List<SimpleGrantedAuthority> PREMIUM_AUTHORITIES = List.of(
+            new SimpleGrantedAuthority("ROLE_USER"),
+            new SimpleGrantedAuthority(ROLE_PREMIUM)
+    );
+
+    public static List<SimpleGrantedAuthority> getAuthoritiesByPlan(String planType) {
+        return switch (planType.toLowerCase()) {
+            case "premium" -> PREMIUM_AUTHORITIES;
+            case "basic" -> BASIC_AUTHORITIES;
+            default -> List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        };
+    }
+}
