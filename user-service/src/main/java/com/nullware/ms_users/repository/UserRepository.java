b96bd4ba07a199abb3b9a14606320c3968b0c7b8
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-13 23:04:16 -0300

    feat: added UserRepository

---
+++ user-service/src/main/java/com/nullware/ms_users/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nullware.ms_users.repository;
+
+import com.nullware.ms_users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
