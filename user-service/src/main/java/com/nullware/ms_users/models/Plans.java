a7fd8140c852516230b106eccd1584e69d8cd46c
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-15 18:06:40 -0300

    refactor: added ROLE_USER

--- user-service/src/main/java/com/nullware/ms_users/models/Plans.java
+++ user-service/src/main/java/com/nullware/ms_users/models/Plans.java
@@ -7,14 +7,15 @@ import java.util.List;
 public class Plans {
     public static final String ROLE_BASIC = "ROLE_BASIC";
     public static final String ROLE_PREMIUM = "ROLE_PREMIUM";
+    public static final String ROLE_USER = "ROLE_USER";
 
     public static final List<SimpleGrantedAuthority> BASIC_AUTHORITIES = List.of(
-            new SimpleGrantedAuthority("ROLE_USER"),
+            new SimpleGrantedAuthority(ROLE_USER),
             new SimpleGrantedAuthority(ROLE_BASIC)
     );
 
     public static final List<SimpleGrantedAuthority> PREMIUM_AUTHORITIES = List.of(
-            new SimpleGrantedAuthority("ROLE_USER"),
+            new SimpleGrantedAuthority(ROLE_USER),
             new SimpleGrantedAuthority(ROLE_PREMIUM)
     );
 
@@ -22,7 +23,7 @@ public class Plans {
         return switch (planType.toLowerCase()) {
             case "premium" -> PREMIUM_AUTHORITIES;
             case "basic" -> BASIC_AUTHORITIES;
-            default -> List.of(new SimpleGrantedAuthority("ROLE_USER"));
+            default -> List.of(new SimpleGrantedAuthority(ROLE_USER));
         };
     }
 }
