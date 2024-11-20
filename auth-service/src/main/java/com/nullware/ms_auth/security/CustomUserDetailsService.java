a91092590ef642198919bcaa9c63e30d8a29e9ca
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-20 16:21:50 -0300

    refactor: removed lombok dependency

--- auth-service/src/main/java/com/nullware/ms_auth/security/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/nullware/ms_auth/security/CustomUserDetailsService.java
@@ -3,15 +3,17 @@ package com.nullware.ms_auth.security;
 import com.nullware.ms_auth.entity.User;
 import com.nullware.ms_auth.models.Plans;
 import com.nullware.ms_auth.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class CustomUserDetailsService {
     final UserRepository userRepo;
+
+    public CustomUserDetailsService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
 
     public UserDetails loadUserByUsername(String username) {
         User user = this.userRepo.findByEmail(username)
