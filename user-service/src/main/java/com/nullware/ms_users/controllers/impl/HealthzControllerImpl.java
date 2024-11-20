014efacd41dd65b6ae6382008a21e4f54b5b87b5
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-20 19:06:17 -0300

    feat: added healthz route

--- user-service/src/main/java/com/nullware/ms_users/controllers/impl/HealthzControllerImpl.java
+++ user-service/src/main/java/com/nullware/ms_users/controllers/impl/HealthzControllerImpl.java
@@ -1,7 +1,7 @@
 package com.nullware.ms_users.controllers.impl;
 
 import com.nullware.ms_users.controllers.HealthzController;
-import com.nullware.ms_users.dtos.responses.GenericResponse;
+import com.nullware.ms_users.dtos.responses.GenericResponseDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthzControllerImpl implements HealthzController {
 
     @Override
-    public ResponseEntity<GenericResponse> healthz() {
-        return ResponseEntity.ok(new GenericResponse("OK"));
+    public ResponseEntity<GenericResponseDTO> healthz() {
+        return ResponseEntity.ok(new GenericResponseDTO("OK"));
+    }
+
+    @Override
+    public ResponseEntity<GenericResponseDTO> authHealthz() {
+        return ResponseEntity.ok(new GenericResponseDTO("User Authenticated"));
     }
 }
