b49a47c1170d41df04b2887f846ef26c766d2a63
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-11-28 18:23:51 -0300

    build: next.js build files

---
+++ null-app/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Button>Hello</Button>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
